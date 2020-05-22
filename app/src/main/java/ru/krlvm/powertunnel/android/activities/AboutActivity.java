3a5ae0621719685269b068075bdebd06a61f558d
krlvm <51774833+krlvm@users.noreply.github.com>
2020-05-22 23:56:07 +0300

    Add link to the MIT License

--- app/src/main/java/ru/krlvm/powertunnel/android/activities/AboutActivity.java
+++ app/src/main/java/ru/krlvm/powertunnel/android/activities/AboutActivity.java
@@ -1,7 +1,9 @@
 package ru.krlvm.powertunnel.android.activities;
 
 import android.app.ProgressDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +23,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        ((TextView) findViewById(R.id.about_version)).setText(getString(R.string.about_version, BuildConfig.VERSION_NAME));
+        TextView version = findViewById(R.id.about_version);
+        String versionText = getString(R.string.about_version, BuildConfig.VERSION_NAME);
+        System.out.println(versionText);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            version.setText(Html.fromHtml(versionText, Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            version.setText(Html.fromHtml(versionText));
+        }
+        version.setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView) findViewById(R.id.about_description)).setText(R.string.description);
         ((TextView) findViewById(R.id.about_powertunnel)).setMovementMethod(LinkMovementMethod.getInstance());
         Button button = findViewById(R.id.updates_button);
