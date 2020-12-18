f00a76c3853ed15e03a0ba60b78d228e3d3e939c
krlvm <51774833+krlvm@users.noreply.github.com>
2020-12-18 22:47:13 +0300

    Fix mismatch in locales, fix UTF-8 support on old devices

--- app/src/main/java/ru/krlvm/powertunnel/utilities/HttpUtility.java
+++ app/src/main/java/ru/krlvm/powertunnel/utilities/HttpUtility.java
@@ -4,7 +4,7 @@ import android.util.Base64;
 
 import org.littleshoot.proxy.impl.ProxyUtils;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.Date;
 
 import io.netty.buffer.ByteBuf;
@@ -21,6 +21,12 @@ import io.netty.handler.codec.http.HttpVersion;
  * @author krlvm
  */
 public class HttpUtility {
+
+    // Android Studio loves to replace Charset.forName("UTF-8")
+    // with StandardCharsets.UTF-8 which is not compatible
+    // with older Android versions, so we use Charset.forName("UTF-8" + "")
+    // to avoid warnings and auto-replace, because this warning can not be suppressed
+    // in the standard way
 
     /**
      * Retrieves stub with reason in a HTML-body
@@ -56,7 +62,7 @@ public class HttpUtility {
      */
     public static HttpResponse getResponse(String html) {
         String body = "<!DOCTYPE html>\n" + html;
-        byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+        byte[] bytes = body.getBytes(Charset.forName("UTF-8" + ""));
         ByteBuf content = Unpooled.copiedBuffer(bytes);
         HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_GATEWAY, content);
         response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, bytes.length);
@@ -81,7 +87,7 @@ public class HttpUtility {
     public static String generateAuthCode(String username, String password) {
         String credential = username + ":" + password;
         byte[] data;
-        data = credential.getBytes(StandardCharsets.UTF_8);
+        data = credential.getBytes(Charset.forName("UTF-8" + ""));
         return Base64.encodeToString(data, Base64.DEFAULT).trim();
     }
 }
